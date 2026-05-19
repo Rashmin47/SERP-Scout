94aa23b5bf055953974050300656cf8e2041dfca
Rashmin <rashminxperience@gmail.com>
2026-05-19 22:51:23 +0545

    added mongo db

--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
 import express from "express";
 import cors from "cors";
+import connectDB from "./config/db.js";
+import "dotenv/config";
+
+connectDB();
 const app = express();
 app.use(cors());
 app.use(express.json());
