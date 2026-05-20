1ea5ce5ade4924384bd71d87e7702d4ca54c19e1
Rashmin <rashminxperience@gmail.com>
2026-05-20 22:35:47 +0545

    added auth router

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { getUser, login, register } from "../controllers/authController";
+import auth from "../middleware/auth";
+const authRouter = express.Router();
+authRouter.post("/register", register);
+authRouter.post("/login", login);
+authRouter.post("/user", auth, getUser);
+export default authRouter;
