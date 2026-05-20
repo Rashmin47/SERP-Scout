b15f0896f38b23d585fd7e33cca6e1c67f627d07
Rashmin <rashminxperience@gmail.com>
2026-05-20 22:37:15 +0545

    updated server.js with authRouter

--- server/server.js
+++ server/server.js
@@ -2,11 +2,13 @@ import express from "express";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import "dotenv/config";
+import authRouter from "./routes/authRoutes.js";
 
 connectDB();
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.get("/", (req, res) => res.send("Server is running"));
+app.use("/api/auth", authRouter);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
